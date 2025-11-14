c2061ee1dac4062d04ae246182a0ff35456f944d
Greg Fisher <greg@gnfisher.com>
2025-11-13 20:36:59 -0500

    Cleanup keymaps

--- shared/.config/nvim/plugin/keymaps.lua
+++ shared/.config/nvim/plugin/keymaps.lua
@@ -3,12 +3,20 @@ local opts = { noremap = true, silent = true }
 
 set("n", "<C-space>", ":noh<CR>", opts)
 
+set({"i", "v"}, "<C-space>", "<Esc>", opts)
+
+
+-- Paste from clipboard
 set("x", "<Leader>p", [["_dP]], opts)
+
+-- Yank to clipboard
 set({ "n", "v" }, "<Leader>y", [["+y]], opts)
 set("n", "<Leader>Y", [["+Y]], opts)
 
+-- Move highlighted lines up/down
 set("v", "J", ":m '>+1<CR>gv=gv", opts)
 set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
+-- Quickfix jump
 set("n", "]q", "<Cmd>cnext<CR>zz", opts)
 set("n", "[q", "<Cmd>cprev<CR>zz", opts)
