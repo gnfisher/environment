b5320a456fc752965ca27d51649655c40466531c
Greg Fisher <greg@gnfisher.com>
2025-11-16 14:52:15 -0500

    More maps for lsp

--- shared/.config/nvim/lua/plugins/lsp.lua
+++ shared/.config/nvim/lua/plugins/lsp.lua
@@ -32,17 +32,14 @@ return {
 
           vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
           vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+          vim.keymap.set("n", "<F12>", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
           vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
           vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
           vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
           vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+          vim.keymap.set("n", "<S-F12>", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
           vim.keymap.set("i", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-          vim.keymap.set(
-            "n",
-            "<leader>vd",
-            "<cmd>lua vim.diagnostic.open_float()<cr>",
-            { desc = "View Diagnostics" }
-          )
+          vim.keymap.set( "n", "<leader>vd", "<cmd>lua vim.diagnostic.open_float()<cr>", { desc = "View Diagnostics" })
           vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
           vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 
