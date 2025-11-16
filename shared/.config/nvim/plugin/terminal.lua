0d0600d3ee270285b16c71bee69bc305c9931ea3
Greg Fisher <greg@gnfisher.com>
2025-11-16 17:08:55 -0500

    Also steal terminal configs from tjdevries

--- shared/.config/nvim/plugin/terminal.lua
+++ shared/.config/nvim/plugin/terminal.lua
@@ -1,7 +1,26 @@
-local set = vim.keymap.set
-local opts = { noremap = true, silent = true }
+local set = vim.opt_local
 
--- Escape terminal with Ctrl-\ or Esc
-set({"t"}, "<C-\\>", "<C-\\><C-n>", opts)
-set("t", "<Esc>", "<C-\\><C-n>", opts)
+-- Set local settings for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    set.number = false
+    set.relativenumber = false
+    set.scrolloff = 0
+
+    vim.bo.filetype = "terminal"
+  end,
+})
+
+-- Easily hit escape in terminal mode.
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+-- Open a terminal at the bottom of the screen with a fixed height.
+vim.keymap.set("n", "<Leader>st", function()
+  vim.cmd.new()
+  vim.cmd.wincmd "J"
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
+end)
 
