60a22809ee2ab377beb7f5a1de3128201beca904
Greg Fisher <greg@gnfisher.com>
2025-11-21 21:22:04 -0500

    Fixed!

--- shared/.config/nvim/lua/plugins/theme.lua
+++ shared/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,12 @@
 return {
---   {
---     -- "tjdevries/colorbuddy.nvim",
---     -- lazy = false,
---     -- priority = 1002,
---     -- config = function()
---     --   -- local colorbuddy = require('colorbuddy')
---     --   -- colorbuddy.colorscheme("gruvbuddy")
---     --   -- vim.cmd.colorscheme("gruvbuddy")
---     -- end,
---   },
+  {
+    "tjdevries/colorbuddy.nvim",
+    lazy = false,
+    priority = 1002,
+    config = function()
+      local colorbuddy = require('colorbuddy')
+      colorbuddy.colorscheme("gruvbuddy")
+      vim.cmd.colorscheme("gruvbuddy")
+    end,
+  },
 }
