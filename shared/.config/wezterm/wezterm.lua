f41e42d37ef351957486b5a5506b5f1c156cf53e
Greg Fisher <greg@gnfisher.com>
2026-01-08 09:38:52 -0500

    Wez term, smea

---
+++ shared/.config/wezterm/wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Appearance
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14.0
+config.line_height = 1.1
+
+-- Window
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 10,
+}
+config.hide_tab_bar_if_only_one_tab = true
+config.native_macos_fullscreen_mode = true
+
+-- Behavior
+config.scrollback_lines = 10000
+config.enable_scroll_bar = false
+config.audible_bell = "Disabled"
+
+-- Keys (keep defaults, add a few conveniences)
+config.keys = {
+  -- Split panes (similar to tmux with C-s)
+  { key = "d", mods = "CMD", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "d", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+  -- Navigate panes
+  { key = "h", mods = "CMD|SHIFT", action = wezterm.action.ActivatePaneDirection("Left") },
+  { key = "l", mods = "CMD|SHIFT", action = wezterm.action.ActivatePaneDirection("Right") },
+  { key = "k", mods = "CMD|SHIFT", action = wezterm.action.ActivatePaneDirection("Up") },
+  { key = "j", mods = "CMD|SHIFT", action = wezterm.action.ActivatePaneDirection("Down") },
+
+  -- Close pane
+  { key = "w", mods = "CMD", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+}
+
+return config
