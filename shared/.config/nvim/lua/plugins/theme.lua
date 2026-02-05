67017cd99e97b838602577c5d86b20c16c91a2f4
Greg Fisher <greg@gnfisher.com>
2026-02-05 18:37:57 -0500

    Switch nvim theme to github_dark_dimmed

--- shared/.config/nvim/lua/plugins/theme.lua
+++ shared/.config/nvim/lua/plugins/theme.lua
@@ -1,58 +1,12 @@
 return {
-  -- Atom One - clean, readable dark/light theme
+  -- GitHub theme - dimmed variant
   {
-    "navarasu/onedark.nvim",
+    "projekt0n/github-nvim-theme",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "dark",
-      transparent = false,
-      term_colors = true,
-      code_style = {
-        comments = "italic",
-        keywords = "bold",
-        functions = "none",
-        strings = "none",
-        variables = "none",
-      },
-      diagnostics = {
-        darker = true,
-        undercurl = true,
-      },
-    },
-  },
-
-  -- Keep catppuccin available as fallback
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = true,
-    opts = {
-      flavour = "mocha",
-      transparent_background = false,
-      term_colors = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        telescope = { enabled = true },
-        indent_blankline = { enabled = true },
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        mason = true,
-        mini = { enabled = true },
-        fidget = true,
-        harpoon = true,
-        which_key = true,
-      },
-    },
+    config = function()
+      require("github-theme").setup({})
+      vim.cmd.colorscheme("github_dark_dimmed")
+    end,
   },
 }
