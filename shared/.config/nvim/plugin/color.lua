f842fd7c5ca06911fed1279680fd2fa9ce6769cc
Greg Fisher <greg@gnfisher.com>
2026-02-05 18:48:36 -0500

    Fix color.lua: replace onedark references with github theme

--- shared/.config/nvim/plugin/color.lua
+++ shared/.config/nvim/plugin/color.lua
@@ -2,16 +2,13 @@ local set = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 vim.o.background = "dark"
-vim.cmd.colorscheme("onedark")
 
 set("n", "<F6>", function()
-  local onedark = require("onedark")
   if vim.o.background == "light" then
     vim.o.background = "dark"
-    onedark.setup({ style = "dark" })
+    vim.cmd.colorscheme("github_dark_dimmed")
   else
     vim.o.background = "light"
-    onedark.setup({ style = "light" })
+    vim.cmd.colorscheme("github_light")
   end
-  onedark.load()
 end, opts)
